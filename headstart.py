6606f6082bcd558402b00002272338835d4729ee
larstila <leila.arstila@aalto.fi>
2023-01-29 17:41:48 +0200

    restructured code

--- headstart.py
+++ headstart.py
@@ -2,7 +2,37 @@
 
 import sys, argparse, logging
 import os
-from utils import check_tools, nmap_scan, ffuf_dir_enum, ffuf_sub_enum
+import subprocess
+
+#You can change the used wordlists
+
+dir_wordlist = "/usr/share/dirb/wordlists/big.txt"
+sub_wordlist = "/usr/share/seclists/Discovery/DNS/bitquark-subdomains-top100000.txt"
+
+def run_command(command):
+    os.system(command)
+
+def nmap_scan():
+    logging.info("Starting nmap")
+    run_command(f"nmap -Pn -sV -sC -oN {output_file}.nmap {ip_address}")
+
+def ffuf_dir_enum():
+    logging.info("Starting ffuf direcory enumeration")
+    run_command(f"ffuf -w {dir_wordlist} -u {hostname}/FUZZ -t 100 -o {output_file}.dirs")
+
+def ffuf_sub_enum():
+    logging.info("Starting subdomain enumeration")
+    run_command(f"ffuf -w {sub_wordlist} -u FUZZ.{hostname} -t 100 -o {output_file}.subs")
+
+def check_tools():
+    tools = ['nmap', 'ffuf']
+    for tool in tools:
+        try:
+            subprocess.run([tool, '--version'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            logging.info(f"{tool} is installed.")
+        except subprocess.CalledProcessError:
+            logging.info(f"Error: {tool} is not installed.")
+            sys.exit(1)
 
 def main(args, loglevel):
     logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
@@ -10,23 +40,21 @@ def main(args, loglevel):
     logging.info("Runs nmap, direcory and subdomain enumeration with ffuf and ???")
 
     check_tools()
-    # TODO nmap, ffuf direcories, ffuf subdomains
-
-    # TODO open new terminal windows for each task
-    # TODO write output
+    
     try:
         hostname = args.hostname
     except AttributeError:
         hostname = args.ip_address
+    
     ip_address = args.ip_address
     output_file = args.output
 
     logging.debug("Hostname: %s" %ip_address)
     logging.debug("Output: %s" %output_file)
 
-    nmap_scan(ip_address, output_file)
-    ffuf_dir_enum(hostname, output_file)
-    ffuf_sub_enum(hostname, output_file)
+    nmap_scan()
+    ffuf_dir_enum()
+    ffuf_sub_enum()
     
 if __name__ == '__main__':
     
